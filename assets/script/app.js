6bf40892699fdd87524228bfde0a39e30c9d66ad
Evan Kennedy <evanckennedy@protonmail.com>
2024-04-01 13:20:21 -0500

    create basic start and restart function

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,7 +4,8 @@
 // and CORS issues
 import * as utils from './utils.js';
 import Score from './Score.js';
-
+import originalWords from './words.js';
+console.log(originalWords);
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Selectors, Declaration                               */
@@ -12,8 +13,8 @@ import Score from './Score.js';
 const start = utils.select('.start');
 const restart = utils.select('.restart');
 const timeRemaining = utils.select('.time-remaining');
-
-let scores = [];
+const hits = utils.select('.hits');
+const input = utils.select('.user-guess');
 
 const gameSound = new Audio('./assets/media/game-sound.mp3');
 const correctAnswer = new Audio('./assets/media/correct-answer.mp3');
@@ -21,51 +22,17 @@ const correctAnswer = new Audio('./assets/media/correct-answer.mp3');
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Random Words                                         */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-const orignalWords = [
-  'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
-  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
-  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
-  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
-  'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
-  'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
-  'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
-  'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
-  'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
-  'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
-  'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
-  'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
-  'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
-  'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
-  'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
-  'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
-  'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
-  'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
-  ];
-let words = [...orignalWords];
+let words = [...originalWords];
 
 function selectWord() {
 let index = Math.floor(Math.random() * words.length);
-let word = words[index]
-
+let word = words[index];
 words.splice(index, 1);
 return word;
 }
 
 function resetWords() {
-words = [...orignalWords];
-}
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Score                                                */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-function setScoreObj() {
-  let score = hitNum;
-  let date = new Date().toDateString();
-  let percentage = ((score / words.length) * 100).toFixed(1);
-
-  let scoreObj = new Score(date, score, percentage);
-
-  scores.push(scoreObj);
+words = [...originalWords];
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
@@ -90,6 +57,58 @@ function startCount() {
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Start Game                                           */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+function hideStart() {
+  start.classList.add('hidden');
+  restart.classList.remove('hidden');
+}
+
+function hideRestart() {
+  restart.classList.add('hidden');
+  start.classList.remove('hidden')
+}
+
+function startGame() {
+  startCount();
+  gameSound.play();
+  input.focus();
+  hideStart();
+}
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Restart Game                                         */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+function restartGame() {
+  resetWords();
+  count = 100;
+  gameSound.currentTime = 0;
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Event Listeners                                      */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+utils.listen('click', start, startGame);
+utils.listen('click', restart, restartGame);
+utils.listen('input', document, )
+
+
+
+/* Use later */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Score                                                */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+let scores = [];
+function setScoreObj() {
+  let score = hitNum;
+  let date = new Date().toDateString();
+  let percentage = ((score / words.length) * 100).toFixed(1);
+
+  let scoreObj = new Score(date, score, percentage);
+
+  scores.push(scoreObj);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Date                                                 */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 function getDate() {
@@ -101,15 +120,3 @@ function getDate() {
 
   return new Date().toLocaleDateString('en-ca', options);
 }
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Start Game                                           */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-function startGame() {
-  startCount()
-}
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Event Listeners                                      */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-utils.listen('click', start, startGame);

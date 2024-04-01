627261fd7fa492931a2d724cd06b038658fd58b0
Evan Kennedy <evanckennedy@protonmail.com>
2024-04-01 08:46:58 -0500

    add random words and word selectors

--- assets/script/app.js
+++ assets/script/app.js
@@ -18,4 +18,39 @@ let scores = [];
 const gameSound = new Audio('./assets/media/game-sound.mp3');
 const correctAnswer = new Audio('./assets/media/correct-answer.mp3');
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Random Words                                         */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const orignalWords = [
+  'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+  'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
+  'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
+  'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
+  'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
+  'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
+  'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
+  'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
+  'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
+  'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
+  'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+  'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
+  'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
+  'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
+  'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
+  ];
+let words = [...orignalWords];
 
+function selectWord() {
+let index = Math.floor(Math.random() * words.length);
+let word = words[index]
+
+words.splice(index, 1);
+return word;
+}
+
+function resetWords() {
+words = [...orignalWords];
+}

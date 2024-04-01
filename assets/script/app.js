42e676e6ad785660c1a5c4091e7622913c44def8
Evan Kennedy <evanckennedy@protonmail.com>
2024-04-01 09:17:16 -0500

    create function to get the date

--- assets/script/app.js
+++ assets/script/app.js
@@ -90,6 +90,19 @@ function startCount() {
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Date                                                 */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+function getDate() {
+  const options = {
+    year: numeric,
+    month: short,
+    day: 2-digit
+  }
+
+  return new Date().toLocaleDateString('en-ca', options);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  Start Game                                           */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 function startGame() {

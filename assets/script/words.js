e0d70bacc3267de8a3c8dd3bf0ab60951f47e2e3
Evan Kennedy <evanckennedy@protonmail.com>
2024-04-01 22:20:09 -0500

    update words.js

--- assets/script/words.js
+++ assets/script/words.js
@@ -1,22 +1,16 @@
-const originalWords = [
-  'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
-  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
-  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
-  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
-  'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
-  'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
-  'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
-  'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
-  'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
-  'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
-  'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
-  'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
-  'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
-  'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
-  'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
-  'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
-  'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
-  'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
-];
+const originalWords = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
+'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
+'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
+'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
+'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
+'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 'agency',
+'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution',
+'banana', 'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music',
+'eagle', 'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button',
+'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
+'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
+'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+'keyboard', 'window'];
 
 export default originalWords;
